802ec6f17f7ecece00d8259ea5d88c555208cdfe
Asaf <asafsilman@gmail.com>
2021-05-21 11:17:35 +0800

    Added mock contracts

---
+++ contracts/mock/ERC20Mock.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.8.0;
+
+// interfaces
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ERC20Mock is ERC20 {
+  constructor(
+    string memory name,
+    string memory symbol,
+    uint256 _creatorSupply
+  )
+    ERC20(name, symbol) public {
+    _mint(address(this), 10**24); // 1,000,000
+    _mint(msg.sender, _creatorSupply);
+  }
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,6 @@
 require("@nomiclabs/hardhat-vyper");
+require("@nomiclabs/hardhat-waffle");
+
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
