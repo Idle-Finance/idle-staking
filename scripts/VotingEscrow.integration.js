6446c991372f6383ddb929fbfad16fe66cdc5dda
Asaf <asafsilman@gmail.com>
2021-05-28 13:56:57 +0800

    Added Integration test for stakedIdle

--- scripts/VotingEscrow.integration.js
+++ scripts/VotingEscrow.integration.js
@@ -1,6 +1,7 @@
-const { sudo, getLockDuration } = require('./helpers')
+const { sudo, getLockDuration, check, checkAproximate } = require('./helpers')
 const { idle, personWithIdle } = require('./addresses.integration')
-const { ethers, network } = require('hardhat')
+const { ethers, network } = require('hardhat');
+const { time } = require('@openzeppelin/test-helpers');
 
 const toWei = ethers.utils.parseEther;
 
@@ -12,16 +13,24 @@ async function main() {
     return
   }
 
-  let idleContract = await ethers.getContractAt('ERC20', idle)
+  let deployer = (await ethers.getSigners())[0]
+  let idleContract = await ethers.getContractAt('ERC20MockWithDelegate', idle)
   let stakedIdle = await (await ethers.getContractFactory('VotingEscrow')).deploy(
     idleContract.address, // Staking Token
     'StakedIDLE', // Token Name
     'stkIDLE', // Token Symbol
     '1.0', // Token Version
-    (await ethers.getSigners())[0].address
+    deployer.address
   )
   console.log("Deployed staked Idle")
+  await deployer.sendTransaction({
+    to: personWithIdle,
+    value: ethers.utils.parseEther("1.0")
+  });
 
+  let idleDelegate = await idleContract.delegates(stakedIdle.address)
+  check(idleDelegate, deployer.address, 'Contract sets delegate')
+  
   let [idleContractAsStaker, staker] = await sudo(personWithIdle, idleContract)
   await idleContractAsStaker.approve(stakedIdle.address, toWei('1000'))
   console.log("Approved idle for staking")
@@ -31,6 +40,17 @@ async function main() {
   // hardhat estimates a higher gas limit than what is required
   await stakedIdle.connect(staker).create_lock(toWei('1000'), lockEnd, {gasLimit: BLOCK_GAS_LIMIT})
   console.log("Created lock")
+
+  let stakedBalance
+  let stakerAddress = await staker.getAddress()
+  stakedBalance = await stakedIdle['balanceOf(address)'](stakerAddress)
+  checkAproximate(stakedBalance, toWei('250'), "stkIdle balance at lock start should be ~ 250")
+
+  time.increase(time.duration.years(1))
+  stakedBalance = await stakedIdle['balanceOf(address)'](stakerAddress)
+  check(stakedBalance, toWei('0'), "stkIdle balance at lock end should be ~ 0")
+
+  await stakedIdle.connect(staker).withdraw({gasLimit: 12450000})
 }
 
 // and properly handle errors.
