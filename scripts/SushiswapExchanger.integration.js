a5edff5443d68a132f4f6a5b253b7b0b30c7fbc3
Asaf <asafsilman@gmail.com>
2021-06-13 16:48:01 +0800

    Added integration test for sushiswap exchanger

--- scripts/SushiswapExchanger.integration.js
+++ scripts/SushiswapExchanger.integration.js
@@ -1,8 +1,8 @@
-const { ethers, network } = require('hardhat')
-const { idle, weth, personWithIdle, personWithWETH } = require('./addresses.integration')
+const { ethers, network, upgrades } = require('hardhat')
+const { idle, weth, sushiswapRouter02, personWithIdle, personWithWETH } = require('./addresses.integration')
 const idleABI = require('../abi/idle.json')
 const { time } = require('@openzeppelin/test-helpers')
-const { sudo, getLockDuration, checkAproximate } = require('./helpers')
+const { sudo, getLockDuration, checkAproximate, check } = require('./helpers')
 
 const toWei = ethers.utils.parseEther
 const toEtherBN = (x) => ethers.BigNumber.from(x.toString())
@@ -11,35 +11,43 @@ const toWeek = (x) => toEtherBN(x).div(WEEK).mul(WEEK)
 BLOCK_GAS_LIMIT = 12450000
 WEEK = 7 * 86400
 
+
 async function main() {
   if (network.name != 'hardhat') {
     console.error('Integration test can only run on hardhat network')
     return
   }
 
-  let deployer = (await ethers.getSigners())[0]
-  let idleContract = await ethers.getContractAt(idleABI, idle)
-  let wethContract = await ethers.getContractAt('ERC20', weth)
-  let stakedIdle = await (await ethers.getContractFactory('VotingEscrow')).deploy(
+  const deployer = (await ethers.getSigners())[0]
+  const idleContract = await ethers.getContractAt(idleABI, idle)
+  const wethContract = await ethers.getContractAt('ERC20', weth)
+  const stakedIdle = await (await ethers.getContractFactory('VotingEscrow')).deploy(
     idleContract.address, // Staking Token
     'StakedIDLE', // Token Name
     'stkIDLE', // Token Symbol
     '1.0', // Token Version
     deployer.address
   )
-  console.log("Deployed staked Idle")
+  console.log(`Deployed staked Idle to ${stakedIdle.address}`)
 
-  startTime = toEtherBN(await time.latest())
-  let feeDistributor = await (await ethers.getContractFactory('FeeDistributor')).deploy(
+  let startTime = toEtherBN(await time.latest())
+  const feeDistributor = await (await ethers.getContractFactory('FeeDistributor')).deploy(
     stakedIdle.address,
     startTime,
-    wethContract.address,
+    idleContract.address,
     deployer.address,
     deployer.address
   )
   console.log(`Deployed feeDistributor contract to ${feeDistributor.address}`)
   await feeDistributor.toggle_allow_checkpoint_token()
-  await feeDistributor.checkpoint_token()
+
+  const SushiswapExchanger = await ethers.getContractFactory("SushiswapExchanger")
+  const sushiswapExchanger = await upgrades.deployProxy(
+    SushiswapExchanger,
+    [sushiswapRouter02, weth, idle, feeDistributor.address])
+  console.log(`Deployed sushiswapExchanger contract to ${sushiswapExchanger.address}`)
+
+  await sushiswapExchanger.connect(deployer).addExchanger(deployer.address)
 
   let [idleContractAsStaker, staker] = await sudo(personWithIdle, idleContract)
   let [wethContractAsOwner] = await sudo(personWithWETH, wethContract)
@@ -62,17 +70,27 @@ async function main() {
   await time.increaseTo(nextWeek.toString()) // advance to next week
 
   await feeDistributor.checkpoint_token()
-  await wethContractAsOwner.transfer(feeDistributor.address, toWei('100'))
+
+  console.log("Transfering 10 WETH to feeExchanger")
+  await wethContractAsOwner.transfer(sushiswapExchanger.address, toWei('10'))
+  let idleOut = await sushiswapExchanger.callStatic.exchange(toWei('10'), toWei('1'))
+  await sushiswapExchanger.exchange(toWei('10'), idleOut)
+  console.log(`Executed exchange. For ${ethers.utils.formatUnits(idleOut)} IDLE`)
+  
+  // manually checkpoint token.
+  // If this call was not made, rewards would be split between 2 weeks.
+  // In production this wouldn't happen because 'checkpoint_token' is called within each week
   await feeDistributor.checkpoint_token()
 
-  await time.increase(time.duration.weeks(1)) // advance by 1 week 
-  // await feeDistributor.checkpoint_token()
+  await time.increase(time.duration.weeks(1)) // advance by 1 week, in order to claim tokens
+  await feeDistributor.checkpoint_token()
   console.log("Distributed fees to feeDistributor")
 
-  console.log((await wethContract.balanceOf(stakerAddress)).toString())
+  let balance0 = await idleContract.balanceOf(stakerAddress)
   await feeDistributor.connect(staker)['claim()']()
-  console.log((await wethContract.balanceOf(stakerAddress)).toString())
+  let balance1 = await idleContract.balanceOf(stakerAddress)
 
+  check(balance1.sub(balance0), idleOut, "Change in stakers idle balance increased after claim")
 }
 
 // and properly handle errors.
--- scripts/addresses.integration.js
+++ scripts/addresses.integration.js
@@ -1,6 +1,7 @@
 module.exports = {
   idle: '0x875773784af8135ea0ef43b5a374aad105c5d39e',
   weth: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+  sushiswapRouter02: '0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F',
   feeCollector: '0xBecC659Bfc6EDcA552fa1A67451cC6b38a0108E4',
   whitelistedAccount: '0xb3c8e5534f0063545cbbb7ce86854bf42db8872b',
   personWithIdle: '0x2bc44f6c34d9200c258ac07a56d81963d4f92c4c',
