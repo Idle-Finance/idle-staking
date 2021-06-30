bd519225cd0893fd2b57f94a0d7d147f691ad881
Asaf <asafsilman@gmail.com>
2021-07-01 00:27:57 +0800

    Log dates

--- tasks/topUpFeeDistributor.js
+++ tasks/topUpFeeDistributor.js
@@ -1,6 +1,19 @@
 const idleABI = require('../abi/idle.json')
 WEEK = 7 * 86400
 
+function timeConverter(timestamp){
+  var a = new Date(parseInt(timestamp.toString()) * 1000);
+  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  var year = a.getFullYear();
+  var month = months[a.getMonth()];
+  var date = a.getDate();
+  var hour = a.getHours();
+  var min = a.getMinutes();
+  var sec = a.getSeconds();
+  var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+  return "Current Time: " + time;
+} 
+
 module.exports = task("topup", "Adds IDLE to fee distributor and increases time to next week")
   .addOptionalParam("amount", "Amount of idle to add", "100")
   .addOptionalParam("feedistributor", "FeeDistributor Address", "")
@@ -31,6 +44,7 @@ module.exports = task("topup", "Adds IDLE to fee distributor and increases time 
     const toEtherBN = (x) => ethers.BigNumber.from(x.toString())
     const toWeek = (x) => toEtherBN(x).div(WEEK).mul(WEEK)
 
+    console.log(timeConverter(await time.latest()))
     await time.increase(time.duration.days(1))
     await FeeDistributor.checkpoint_token()
 
@@ -38,12 +52,14 @@ module.exports = task("topup", "Adds IDLE to fee distributor and increases time 
     let currentWeek = await toWeek(currentTime)
     let nextWeek = currentWeek.add(WEEK)
     await time.increaseTo(nextWeek.toString()) // advance to next week
+    console.log(timeConverter(await time.latest()))
 
     await FeeDistributor.checkpoint_token()
     await idleContract.connect(personWithIdleSigner).transfer(FeeDistributor.address, toWei(amount))
-    await time.increase(time.duration.days(2))
+    await time.increase(time.duration.days(1))
     await FeeDistributor.checkpoint_token()
     
     await time.increase(time.duration.weeks(1)) // advance by 1 week 
     console.log(`Added ${amount} $IDLE to FeeDistributor, and updated checkpoint`)
+    console.log(timeConverter(await time.latest()))
   })
